430167b42bbdf93ac9bc914396a3c94f85fc7b1e
hermanboxcar5 <95950881+hermanboxcar5@users.noreply.github.com>
2023-02-24 20:39:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,74 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+var http = require("http");
+var Unblocker = require("unblocker");
+
+var unblocker = Unblocker({});
+
+http
+  .createServer(function(req, res) {
+    unblocker(req, res, function(err) {
+      var headers = { "content-type": "text/html" };
+      if (err) {
+        res.writeHead(500, headers);
+        return res.end(err.stack || err);
+      }
+      if (req.url == "/") {
+        res.writeHead(200, headers);
+        return res.end(
+          `<style>
+            html, body {
+              margin: 0;
+              padding: 0;
+              background-color: #000015;
+              color: #eeeef9;
+              font-family: sans-serif;
+            }
+            div#cont {
+              width: 50%;
+              height: 50%;
+              margin: auto;
+              margin-top: 10%;
+              text-align: center;
+            }
+            input {
+              padding: 8px;
+              background-color: #101025;
+              border: none;
+              border-radius: 4px;
+              color: #dedee9;
+            }
+            input#sub:hover {
+              cursor: hand;
+              background-color: #202035;
+              transition-duration: 300ms;
+            }
+            div#desc {
+              width: 100%;
+              text-align: justified;
+              margin: 2%;
+            }
+            </style>
+            <script>
+              function loadProxy(url){
+                url = url.replace('//', '/')
+                window.location.href = 'https://Proxy.ttrmaniac.repl.co/proxy/' + url;
+              }
+            </script>
+            <body>
+            <div id='cont'>
+              <h2>Proximity v1.25 Titanium</h2>
+              <form onsubmit="return false;">
+                <input id='url' type='url' placeholder='URL' autofocus></input>
+                <input id = 'sub' type='submit' value='Go!' onclick="loadProxy(document.getElementById('url').value)"></input>
+              </form>
+            </div>
+            </body>`
+        );
+      } else {
+        res.writeHead(404, headers);
+        return res.end("ERROR 404: File Not Found.");
+      }
+    });
+  })
+  .listen(8080);
+
+console.log("Rroxy Punning Guccesfully, Sreat");
